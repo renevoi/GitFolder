691e03f6c034230858b14f2b9f042807c93dbc89
rene <rene.catapang@gmail.com>
2017-10-25 12:05:49 +0800

    add another print

---
+++ TestJava.java
@@ -0,0 +1,9 @@
+public class TestJava{
+
+   public static void main(String[] args) {
+        
+        System.out.println("Print Java Text");
+        System.out.println("Another print...");
+
+   }             
+}
